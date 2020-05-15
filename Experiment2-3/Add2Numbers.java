5092502e197174223896c41031ae57325e5a4d8a
Vansh <vansharora2929@gmail.com>
2020-05-15 16:50:09 +0530

    2) Write a program to add two number using command line arguments.

---
+++ Experiment2-3/Add2Numbers.java
@@ -0,0 +1,9 @@
+public class Add2Numbers
+{
+    public static void main(String args[])
+    {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println("Sum of numbers: " + (a+b));
+    }
+}
