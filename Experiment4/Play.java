1f68735c173c6a364baf2e5cd23191adad22eb81
Vansh <vansharora2929@gmail.com>
2020-05-16 20:33:23 +0530

    2) Write a program in Java to create a Player class. Inherit the classes Cricket _Player, Football _Player and Hockey_ Player from Player class.

---
+++ Experiment4/Play.java
@@ -0,0 +1,36 @@
+
+class Player
+{
+    String name;
+}
+
+class CricketPlayer extends Player
+{
+    public void play()
+    {
+        System.out.println("I play Cricket");
+    }
+}
+
+class FootballPlayer extends Player
+{
+    public void play()
+    {
+        System.out.println("I play Football");
+    }
+}
+
+class HockeyPlayer extends Player
+{
+    public void play()
+    {
+        System.out.println("I play Hockey");
+    }
+}
+public class Play
+{
+    public static void main(String args[])
+    {
+        
+    }
+}
